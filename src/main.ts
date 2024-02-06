d3cd7260d926cd8ea980285bd171e006a6a89fbc
hoja1381 <h.jahandideh2@gmail.com>
2024-02-06 11:29:59 +0330

    feat : main file edited.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  app.use(cookieSession({ keys: [process.env.COOKIE_SESSION_KEY] }));
+
+  const config = new DocumentBuilder()
+    .setTitle('store_api')
+    .setDescription('online store')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
