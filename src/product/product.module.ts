60b4c27b819bb9791146f9ba6ba3895dbb22f8f3
hoja1381 <h.jahandideh2@gmail.com>
2024-02-06 15:14:14 +0330

    feat : product service created.

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { ImageModule } from 'src/image/image_providor/image.module';
 
 @Module({
+  imports: [ImageModule],
   controllers: [ProductController],
   providers: [ProductService],
 })
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,26 +1,47 @@
-import { Injectable } from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { DatabaseRepo } from 'src/database/database.service';
+import { ImageService } from 'src/image/image_providor/images.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(private databaseRepo: DatabaseRepo) {}
+
+  async create(data: Prisma.ProductCreateInput) {
+    if (Object.keys(data).length <= 0)
+      throw new BadRequestException('data must be provided.');
+
+    return await this.databaseRepo.product.create({ data: data });
   }
 
-  findAll() {
-    return `This action returns all product`;
+  async findAll() {
+    return await this.databaseRepo.product.findMany({
+      include: { images: true },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    if (!id) throw new BadRequestException('id must be provided');
+
+    return await this.databaseRepo.product.findUnique({
+      where: { id },
+      include: { images: true },
+    });
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, data: Prisma.ProductUpdateInput) {
+    if (Object.keys(data).length <= 0)
+      throw new BadRequestException('data must be provided.');
+
+    if (!id) throw new BadRequestException('id must be provided');
+
+    return await this.databaseRepo.product.update({ where: { id }, data });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    if (!id) throw new BadRequestException('id must be provided');
+
+    return await this.databaseRepo.product.delete({ where: { id } });
   }
 }
