04bbd1f8cf8157561989406ad20829257b39016a
hoja1381 <h.jahandideh2@gmail.com>
2024-02-06 11:26:16 +0330

    feat : user module edited

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
+import { VerifyUser } from './middleware/verify_cookie';
 
 @Module({
   imports: [ConfigModule.forRoot(), DatabaseModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(VerifyUser).forRoutes('*');
+  }
+}
