920928f9ea13567038993977971430ec85b50858
hoja1381 <h.jahandideh2@gmail.com>
2024-02-06 11:31:58 +0330

    feat: user module.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './service/user.service';
 import { UserController } from './controller/user.controller';
+import { AuthService } from './service/auth.service';
+import { AuthController } from './controller/auth.controller';
 
 @Module({
-  controllers: [UserController],
-  providers: [UserService],
+  controllers: [UserController, AuthController],
+  providers: [UserService, AuthService],
+  exports: [UserService, AuthService],
 })
 export class UserModule {}
