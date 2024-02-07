a57ced0a765cd4d8594c33ee18bb9390fd87ac1c
hoja1381 <h.jahandideh2@gmail.com>
2024-02-07 14:54:07 +0330

    feat : bigint to string transformer interceptor added.

---
+++ src/intercepotor/transform_bigint.interceptor.ts
@@ -0,0 +1,35 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+
+export class TransformBigint implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((response) => {
+        const res = { response };
+        console.log(res);
+        const transformedRes = this._convert_bigint_tostring(res);
+        console.log(transformedRes);
+        return transformedRes.response;
+      }),
+    );
+  }
+  private _convert_bigint_tostring(input: any) {
+    if (Array.isArray(input)) {
+      return input.map((item) => this._convert_bigint_tostring(item));
+    } else if (typeof input === 'object' && input !== null) {
+      const result: any = {};
+      for (const key in input) {
+        if (Object.prototype.hasOwnProperty.call(input, key)) {
+          result[key] = this._convert_bigint_tostring(input[key]);
+        }
+      }
+      return result;
+    } else if (typeof input === 'bigint') {
+      return input.toString();
+    }
+    return input;
+  }
+}
