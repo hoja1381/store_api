8f413a14b322c31dc12a851d11c8560d6f153140
hoja1381 <h.jahandideh2@gmail.com>
2024-02-10 15:53:44 +0330

    feat : card routs and controller added.

--- src/card/card.controller.ts
+++ src/card/card.controller.ts
@@ -1,34 +1,72 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ForbiddenException,
+} from '@nestjs/common';
 import { CardService } from './card.service';
 import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
+import { CurrentUser } from 'src/decorator/current_user';
+import { User } from '@prisma/client';
 
-@Controller('card')
+@Controller('cart')
 export class CardController {
   constructor(private readonly cardService: CardService) {}
 
-  @Post()
-  create(@Body() createCardDto: CreateCardDto) {
-    return this.cardService.create(createCardDto);
+  @Post('/')
+  async create(
+    @Body() createCardDto: CreateCardDto,
+    @CurrentUser() user: User,
+  ) {
+    return await this.cardService.create(createCardDto, user?.id);
   }
 
-  @Get()
-  findAll() {
-    return this.cardService.findAll();
+  @Get('/')
+  async findByUser(@CurrentUser() user: User) {
+    return await this.cardService.findByUser(user.id);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.cardService.findOne(+id);
+  @Get('/:id')
+  async findOne(@Param('id') id: string, @CurrentUser() user: User) {
+    const cart = await this.cardService.findOne(+id);
+
+    if (user.id !== cart.user_id)
+      throw new ForbiddenException('you cant access that cart.');
+
+    return cart;
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCardDto: UpdateCardDto) {
-    return this.cardService.update(+id, updateCardDto);
+  @Patch('/addproduct/:id')
+  async addProductToCart(
+    @CurrentUser() user: User,
+    @Param('id') id: string,
+    @Body() updateCardDto: UpdateCardDto,
+  ) {
+    await this.findOne(id, user);
+
+    return await this.cardService.addProductToCart(+id, updateCardDto);
+  }
+
+  @Patch('/removeproduct/:id')
+  async removeProductFromCart(
+    @CurrentUser() user: User,
+    @Param('id') id: string,
+    @Body() updateCardDto: UpdateCardDto,
+  ) {
+    await this.findOne(id, user);
+
+    return await this.cardService.deleteProductFromCart(+id, updateCardDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.cardService.remove(+id);
+  async remove(@Param('id') id: string, @CurrentUser() user: User) {
+    await this.findOne(id, user);
+
+    return await this.cardService.remove(+id);
   }
 }
