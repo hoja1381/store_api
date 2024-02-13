752d99b9c0182ae44e60a7caa4ac3f91931bf2dd
hoja1381 <h.jahandideh2@gmail.com>
2024-02-13 12:06:50 +0330

    feat : set the needed routes in the order carts with there guards.

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -6,38 +6,54 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { CurrentUser } from 'src/common/decorator/current_user';
+import { User } from '@prisma/client';
+import { IsLoggedInGuard } from 'src/common/guard/is_logged_user.guard';
+import { IsAdminGuard } from 'src/common/guard/is_admin.guard';
 
 @Controller('order')
 @ApiTags('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
-  @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
-    return this.orderService.create(createOrderDto);
+  @Post('/')
+  @UseGuards(IsLoggedInGuard)
+  create(@Body() body: CreateOrderDto, @CurrentUser() user: User) {
+    return this.orderService.create(body, user);
   }
 
-  @Get()
+  @Get('/')
+  @UseGuards(IsAdminGuard)
   findAll() {
     return this.orderService.findAll();
   }
 
-  @Get(':id')
+  @Get('/byuser')
+  @UseGuards(IsLoggedInGuard)
+  findByUser(@CurrentUser() user: User) {
+    return this.orderService.findByUser(user.id);
+  }
+
+  @Get('/:id')
+  @UseGuards(IsAdminGuard)
   findOne(@Param('id') id: string) {
     return this.orderService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
-    return this.orderService.update(+id, updateOrderDto);
+  @Patch('/:id')
+  @UseGuards(IsAdminGuard)
+  update(@Param('id') id: string, @Body() body: UpdateOrderDto) {
+    return this.orderService.update(+id, body);
   }
 
-  @Delete(':id')
+  @Delete('/:id')
+  @UseGuards(IsAdminGuard)
   remove(@Param('id') id: string) {
     return this.orderService.remove(+id);
   }
