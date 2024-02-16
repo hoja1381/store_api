b016560a999da80d7a8d20d3e2aaa3295fef6946
hoja1381 <h.jahandideh2@gmail.com>
2024-02-16 15:20:43 +0330

    test : User Module Controller test defind.

--- src/user/controller/auth.controller.spec.ts
+++ src/user/controller/auth.controller.spec.ts
@@ -1,7 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserController } from './user.controller';
-import { UserService } from '../service/user.service';
 import { AuthController } from './auth.controller';
+import { AuthService } from '../service/auth.service';
+import { UserService } from '../service/user.service';
+import { DatabaseRepo } from '../../common/database/database.service';
 
 describe('UserController', () => {
   let controller: AuthController;
@@ -9,7 +10,7 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthController],
+      providers: [AuthService, UserService, DatabaseRepo],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
