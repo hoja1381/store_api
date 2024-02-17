201f83731e7c6f211020ffb4b8f348d17a8f822e
hoja1381 <h.jahandideh2@gmail.com>
2024-02-17 11:09:04 +0330

    fix : export convert big int to string.

--- src/common/interceptor/transform_bigint.interceptor.ts
+++ src/common/interceptor/transform_bigint.interceptor.ts
@@ -1,6 +1,23 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
 import { Observable, map } from 'rxjs';
 
+export function _convert_bigint_tostring(input: any) {
+  if (Array.isArray(input)) {
+    return input.map((item) => _convert_bigint_tostring(item));
+  } else if (typeof input === 'object' && input !== null) {
+    const result: any = {};
+    for (const key in input) {
+      if (Object.prototype.hasOwnProperty.call(input, key)) {
+        result[key] = _convert_bigint_tostring(input[key]);
+      }
+    }
+    return result;
+  } else if (typeof input === 'bigint') {
+    return input.toString();
+  }
+  return input;
+}
+
 export class TransformBigint implements NestInterceptor {
   intercept(
     context: ExecutionContext,
@@ -9,25 +26,9 @@ export class TransformBigint implements NestInterceptor {
     return next.handle().pipe(
       map((response) => {
         const res = { response };
-        const transformedRes = this._convert_bigint_tostring(res);
+        const transformedRes = _convert_bigint_tostring(res);
         return transformedRes.response;
       }),
     );
-  }
-  private _convert_bigint_tostring(input: any) {
-    if (Array.isArray(input)) {
-      return input.map((item) => this._convert_bigint_tostring(item));
-    } else if (typeof input === 'object' && input !== null) {
-      const result: any = {};
-      for (const key in input) {
-        if (Object.prototype.hasOwnProperty.call(input, key)) {
-          result[key] = this._convert_bigint_tostring(input[key]);
-        }
-      }
-      return result;
-    } else if (typeof input === 'bigint') {
-      return input.toString();
-    }
-    return input;
   }
 }
